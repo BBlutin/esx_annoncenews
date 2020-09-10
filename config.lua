a414a6bcd57184a213bf392ea9532b5c82de0813
Thomas <thomas.fritschy@gmail.com>
2020-09-10 16:04:06 +0200

    No more config problem

--- config.lua
+++ config.lua
@@ -1,4 +1,5 @@
 
+Config = {}
 
 Config.debug = true
 
--- src/server.lua
+++ src/server.lua
@@ -10,7 +10,7 @@ ESX.RegisterUsableItem('ticket_news', function(source)
     local xPlayer = ESX.GetPlayerFromId(source)
     local ticket = xPlayer.getInventoryItem('ticket_news')
     if ticket.count >= 1 then
-        if Config.debug then
+        if Config.debug == true then
             print("Inventory check")
         end
 	    xPlayer.removeInventoryItem('ticket_news', 1)
@@ -26,7 +26,7 @@ AddEventHandler('bblutin_annonces:annonceStart', function(result , society)
     local xPlayers = ESX.GetPlayers()
     local text = result
     local titre = society
-    if Config.debug then
+    if Config.debug == true then
         print(society.." : "..text)
     end
     TriggerClientEvent('bblutin_annonces:annonceStart', -1 , text, titre)
